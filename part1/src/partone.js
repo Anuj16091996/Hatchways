903928b29466cac4787bb61b153414c6437e5590
Anuj16091996 <anuj161996@gmail.com>
2022-04-18 23:03:52 -0400

    changes some modictaion in axios library

--- part1/src/partone.js
+++ part1/src/partone.js
@@ -1,32 +1,40 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 
-const FetchAPI = () => {
-  return axios
-    .get("https://randomuser.me/api/")
-    .then((response) => {
-      return response;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-};
+// const FetchAPI = () => {
+//   return axios
+//     .get("https://randomuser.me/api/")
+//     .then((response) => {
+//       return response;
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// };
+
+// FetchAPI().then((data) => {
+//   // console.log(data.data.results);
+//   setUserData(JSON.stringify(data, null, 2));
+// });
 
 export default function PartOne() {
-  const [userData, setUserData] = useState("");
+  const [userData, setUserData] = useState([]);
 
   useEffect(() => {
-    FetchAPI().then((data) => {
-      setUserData(JSON.stringify(data, null, 2));
+    fetch("https://randomuser.me/api/").then((response) => {
+      if (response.ok) {
+        response.json().then((json_response) => {
+          setUserData(json_response.results);
+        });
+      } else {
+      }
     });
   }, []);
 
   return (
     <div>
-      <div>
-        <pre>{userData}</pre>
-      </div>
+      <div>{console.log(userData)}</div>
     </div>
   );
 }
